cdd70e83ed445ed68635a82b58003cad0561733e
Kozlov ALexandr <kozlovalexander21@gmail.com>
2022-01-12 11:54:05 +0300

    Add modul for search files by mask

--- script.ps1
+++ script.ps1
@@ -1,1 +1,14 @@
-﻿
+﻿# Task is a few steps. First step create part code it will find files with mask. Second step create part code. It will change text in files.
+
+$path = "C:\Users\alexandr.kozlov\Desktop\files"
+
+$mask = @("*.txt", "*.doc")
+
+
+#find file
+foreach($M in $mask)
+{
+    Get-ChildItem  -Path $path  -Filter $M
+}
+
+
